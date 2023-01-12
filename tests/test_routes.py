e695f56e97c54b9276fcf56fb611a1fbc09e9373
IsaacSherman <isaac.b.sherman@gmail.com>
2023-01-12 18:58:39 -0500

    all

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -124,3 +124,11 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
     # ADD YOUR TEST CASES HERE ...
+    def test_list_accounts(self):
+        """Accounts should not return 404, should return array of accounts (even empty array)"""
+        account = AccountFactory()
+        response = self.client.get(
+            BASE_URL
+        )
+        self.assertEqual(0, len(response.first))
+        
